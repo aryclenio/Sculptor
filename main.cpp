f5e03115ccd4757702ea7acf5369953251c9c3a0
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2019-04-15 08:35:11 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "matriz.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Matriz a(3,4);
+    a.print();
+    return 0;
+}
---
+++ matriz.h
@@ -0,0 +1,14 @@
+class Matriz{
+private:
+    int nl, nc;
+    int **x;
+public:
+    Matriz(int _nl = 0, int _nc = 0);
+    ~Matriz();
+    void print();
+};
+
+#ifndef MATRIZ_H
+#define MATRIZ_H
+
+#endif // MATRIZ_H
