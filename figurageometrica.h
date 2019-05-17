caee7e8353f89370547156362556b08bd0e9f31d
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2019-05-17 08:22:47 -0300

    Add files via upload

--- figurageometrica.h
+++ figurageometrica.h
@@ -4,8 +4,8 @@
 class FiguraGeometrica{
 
 public:
-    FiguraGeometrica();
+    //FiguraGeometrica();
     virtual void draw(Sculptor &t)=0;
-    virtual  ~FiguraGeometrica();
+    virtual  ~FiguraGeometrica(){};
 };
 #endif // FIGURAGEOMETRICA_H
