8b256494a0b875567b803f7a7776465d3ad71b94
Iago <48588172+IagoCassel@users.noreply.github.com>
2019-06-26 06:09:54 -0300

    Delete classes_2.js

--- docs/search/classes_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['putbox',['PutBox',['../class_put_box.html',1,'']]],
-  ['putellipsoid',['PutEllipsoid',['../class_put_ellipsoid.html',1,'']]],
-  ['putsphere',['PutSphere',['../class_put_sphere.html',1,'']]],
-  ['putvoxel',['PutVoxel',['../class_put_voxel.html',1,'']]]
-];
