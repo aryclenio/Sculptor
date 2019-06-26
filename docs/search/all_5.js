09ce4b36108ad219502e641978723857de5cb01a
Iago <48588172+IagoCassel@users.noreply.github.com>
2019-06-26 06:08:17 -0300

    Delete all_5.js

--- docs/search/all_5.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['putbox',['PutBox',['../class_put_box.html',1,'']]],
-  ['putellipsoid',['PutEllipsoid',['../class_put_ellipsoid.html',1,'']]],
-  ['putsphere',['PutSphere',['../class_put_sphere.html',1,'']]],
-  ['putvoxel',['PutVoxel',['../class_put_voxel.html',1,'PutVoxel'],['../class_sculptor.html#a4bdea3048b419d58e93074060eaa7b52',1,'Sculptor::putVoxel()']]]
-];
