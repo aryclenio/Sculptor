083979e45c6fd6d5c3833dd6975efffa129de66f
Iago <48588172+IagoCassel@users.noreply.github.com>
2019-06-26 06:07:43 -0300

    Delete all_1.js

--- docs/search/all_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['cutbox',['CutBox',['../class_cut_box.html',1,'']]],
-  ['cutellipsoid',['CutEllipsoid',['../class_cut_ellipsoid.html',1,'']]],
-  ['cutsphere',['CutSphere',['../class_cut_sphere.html',1,'']]],
-  ['cutvoxel',['CutVoxel',['../class_cut_voxel.html',1,'CutVoxel'],['../class_sculptor.html#ad9d714a35fc8ae16d06eb5df37c3493c',1,'Sculptor::cutVoxel()']]]
-];
