a45d9c601cfcb1ebc59ea1a09259e41b5f9cc0db
Iago <48588172+IagoCassel@users.noreply.github.com>
2019-06-26 06:09:38 -0300

    Delete classes_0.js

--- docs/search/classes_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['cutbox',['CutBox',['../class_cut_box.html',1,'']]],
-  ['cutellipsoid',['CutEllipsoid',['../class_cut_ellipsoid.html',1,'']]],
-  ['cutsphere',['CutSphere',['../class_cut_sphere.html',1,'']]],
-  ['cutvoxel',['CutVoxel',['../class_cut_voxel.html',1,'']]]
-];
