2a4dd26016d87d7fb4c26131f001192bf0969678
Iago <48588172+IagoCassel@users.noreply.github.com>
2019-06-26 06:08:26 -0300

    Delete all_6.js

--- docs/search/all_6.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['sculptor_20drawer',['Sculptor drawer',['../md__r_e_a_d_m_e.html',1,'']]],
-  ['sculptor',['Sculptor',['../class_sculptor.html',1,'']]],
-  ['setcolor',['setColor',['../class_sculptor.html#af1d69da01379874b0dfd6454787cb562',1,'Sculptor']]]
-];
